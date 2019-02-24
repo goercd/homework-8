23b1e05dbff1bf4e24e2d41dde59d0fa01587efb
goercd <46823854+goercd@users.noreply.github.com>
2019-02-24 22:53:08 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+﻿var promise = new Promise((resolve, reject) => {
+
+setTimeout(() => {
+reject(new Error("время вышло!"));
+}, 1000);
+
+});
+
+promise
+.then(
+result => alert("Fulfilled: " + result),
+error => alert("Rejected: " + error.message) // Rejected: время вышло!
+);
+
+
+
+
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'phones.json', false);
+
+xhr.send();
+
+if (xhr.status != 200) {
+
+alert( xhr.status + ': ' + xhr.statusText ); 
+} else {
+
+alert( xhr.responseText ); 
+}
+    
